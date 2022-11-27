e26fb946f615d782f6266bfc54b0d2d12ea61b18
Giri Raj <girirajsekar50@gmail.com>
2022-11-27 01:28:07 -0400

    Added proxy server

--- src/main/java/com/proxy/Controllers/ProxyServer.java
+++ src/main/java/com/proxy/Controllers/ProxyServer.java
@@ -6,12 +6,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.Proxy;
+import java.net.URL;
+
 @RestController
 @CrossOrigin
 public class ProxyServer {
     @GetMapping("/{resource}")
-    public String proxy(@PathVariable String resource){
-        System.out.println(resource);
+    public String proxy(@PathVariable String resource) throws IOException {
+        String HTTP_ENDPOINT = "localhost:8080";
+        URL weburl = new URL(HTTP_ENDPOINT);
+        HttpURLConnection directConnection
+                = (HttpURLConnection) weburl.openConnection(Proxy.NO_PROXY);
+        directConnectio
         return "Done";
     }
 }
